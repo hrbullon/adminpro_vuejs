8edb3ab40a293c2c367fa4a3cd034597577c5372
Haderson Bullón <hrbullon@gmail.com>
2019-06-06 22:47:40 -0400

    Vue-router installed and tested

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+//import VueResource from 'vue-resource'
 import { routes } from './routes/pages'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+    //Vue.use(VueResource)
 
 const router = new VueRouter({
     routes: routes
